454b54e7300cfcd49ad3623cb6a7645f639565b0
junio-gon <junio-gon@hotmail.com>
2022-05-05 22:25:23 -0300

    criacao da estrutura e primeiros models
    Signed-off-by: junio-gon <junio-gon@hotmail.com>

---
+++ ExemploAspCore/Infraestruture.API/Data/Models/Base.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infraestruture.API.Data.Models
+{
+    public class Base
+    {
+        [Required]
+        public int Id { get; set; } 
+    }
+}
---
+++ ExemploAspCore/Infraestruture.API/Data/Models/Contato.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestruture.API.Data.Models
+{
+    public class Contato : Base
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "O tamanho máximo para o campo é {0}")]
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }  
+        public DateTime? Nascimento { get; set; }
+        public string? Email { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public DateTime? AtualizadoEm { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
